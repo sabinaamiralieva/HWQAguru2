1e9af9822514e5c5c39070073cc86f5525670268
Sabina <sabinnabali@sberhealth.ru>
2022-10-21 16:00:00 +0300

    first

---
+++ src/test/java/org/example/Hw.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Hw {
+
+    @Test
+    public void exampleTest0() {
+        Assertions.assertFalse(2 <= 1);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue(3 >= 2);
+    }
+}
